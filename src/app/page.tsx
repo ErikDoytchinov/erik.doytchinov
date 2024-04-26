6cf95897b9e9b374e335d11bce69cc56a77e6770
Erik Doytchinov <erik.doytchinov@gmail.com>
2024-04-26 23:45:36 +0200

    Add some files or something

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,15 @@ import * as React from 'react';
 import Link from 'next/link'
 import Image from 'next/image'
 
+function calculateTimeBetweenTwoStringDates(date1: string) {
+  const date: any = new Date(date1);
+  const dateNow: any = new Date();
+  const diffTime = Math.abs(dateNow - date);
+  const diffMos = Math.ceil(diffTime / (1000 * 60 * 60 * 24 * 30)); 
+  return diffMos + " mos";
+}
+
+
 export default function MyApp() {
   return ( 
     <main>
@@ -17,12 +26,10 @@ export default function MyApp() {
           />
           <div className="mt-7">
             <Link href="https://github.com/ErikDoytchinov" className="text-2xl text-slate-400">GitHub</Link>
-            {/* <Link href="https://github.com/ErikDoytchinov" className="text-2xl text-slate-400">GitHub</Link> */}
           </div>
 
           <p className="text-white text-left mt-6">CONTACT</p>
           <p className="text-white text-left">erik.doytchinov@gmail.com</p>
-          <p className="text-white text-left">+31 6 84 05 49 44</p>
         </div>
       </aside>
       <div className="fixed top-0 right-0 z-40 w-10/12 h-screen transition-transform -translate-x-full sm:translate-x-0 bg-neutral-900">
@@ -30,7 +37,7 @@ export default function MyApp() {
         <p className="top-10 ml-6 pt-5 left-3 text-white bg-gradient-to-tr:bg-slate-300 text-sm">
           Currently persuing my Bachelor&apos;s in Computer Science (<Link href="https://vu.nl/en/education/bachelor/computer-science" className="text-slate-400">VU Amsterdam</Link>).
           I&apos;m interested in everything tech related, with a focus on software engineering and big data. Here I include all project I&apos;ve tinkered with
-          throughout the years, RaspberryPi and others. I&apos;m also into video games, and 
+          throughout the years, RaspberryPi and others. I&apos;m also into video games, I&apos;m currently playing <Link href="https://www.ea.com/games/apex-legends" className="text-slate-400">Apex Legends</Link> and <Link href="https://www.ea.com/games/battlefield/battlefield-2042" className="text-slate-400">Battlefield 2042</Link>.
         </p>
         <hr className="h-px my-5 bg-gray-200 border-0 dark:bg-gray-700"></hr>
         <h1 className="text-2xl ml-6 pt-1 left-3 text-white">Experiences</h1>
@@ -41,7 +48,7 @@ export default function MyApp() {
         <ul className="text-white list-inside ml-3 pt-2">
             <li className="list-item pt-1 pl-3">
                 <div className="inline text-xs">
-                    <p className="">Software Engineer Intern</p>
+                    <p className="text-base">Software Engineer Intern</p>
                     <p className="text-sm"><Link href="https://www.dvbcontrol.com/" className="text-slate-400">DVBControl</Link> · Internship</p>
                     <p className="text-xs">Aug 2023 - Oct 2023 · 3 mos</p>
                     <ul className="pl-4 pt-1">
@@ -55,9 +62,9 @@ export default function MyApp() {
             </li>
             <li className="list-item pt-1 pl-3">
                 <div className="inline text-xs">
-                    <p className="">Service Associate</p>
-                    <p className="text-sm">SUGO Pizza al Taglio · Part-time</p>
-                    <p className="text-xs">Jan 2023 - Present · 1 yr 1 mo</p>
+                    <p className="text-base">Service Associate</p>
+                    <p className="text-sm"><Link href="https://sugo.nl/" className="text-slate-400">SUGO Pizza al Taglio</Link> · Part-time</p>
+                    <p className="text-xs">Jan 2023 - Present · {calculateTimeBetweenTwoStringDates("2023-01-01")}</p>
                     <ul className="pl-4 pt-1">
                         <li>• Expertly prepared and cooked a wide range of Italian-style pizzas to perfection, ensuring consistency in taste and presentation.</li>
                         <li>• Maintained a clean and organized kitchen, adhering to food safety and hygiene standards. Demonstrated proficiency in working with pizza ovens and other kitchen equipment.</li>
@@ -68,8 +75,8 @@ export default function MyApp() {
             </li>   
             <li className="list-item pt-1 pl-3">
                 <div className="inline text-xs">
-                    <p className="">Customer Service Assistant</p>
-                    <p className="text-sm">Jack Astor&apos;s Bar and Grill · Part-time</p>
+                    <p className="text-base">Customer Service Assistant</p>
+                    <p className="text-sm"><Link href="https://jackastors.com/" className="text-slate-400">Jack Astor&apos;s Bar and Grill</Link> · Part-time</p>
                     <p className="text-xs">Feb 2022 - Sep 2022 · 8 mos</p>
                     <ul className="pl-4 pt-1">
                         <li>• Skilled in delivering outstanding service by greeting and serving customers.</li>
