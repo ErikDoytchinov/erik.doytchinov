1f698f80fa98bd1598d1a0f30300cd5b113720b3
Erik Doytchinov <erik.doytchinov@gmail.com>
2024-05-20 03:30:40 +0200

    Add missing <p>

--- src/erik-doytchinov/App.tsx
+++ src/erik-doytchinov/App.tsx
@@ -6,15 +6,13 @@ function App() {
         <>
             <div className="h-screen flex overflow-hidden">
                 <aside className="w-64 text-white flex-shrink-0 p-4 bg-slate-800">
-                    <h1 className="text-left text-2xl pt-7 pb-5 text-white">
+                    <h1 className="text-left text-2xl pt-2 pb-5 text-white">
                         Erik Doytchinov
                     </h1>
                     <img
                         src="https://media.licdn.com/dms/image/D4E03AQGKkYD_g2UwYw/profile-displayphoto-shrink_400_400/0/1705500073136?e=1720051200&v=beta&t=bqr2OTW8NiezDIWYPRp_FMxSIIAedCMSHipHJq-qC9M"
-                        width={200}
-                        height={200}
                         alt="Picture of the author"
-                        className="rounded-md mb-4 ml-2"
+                        className="rounded-md mb-4"
                     />
                     <div className="mt-7">
                         <a
--- src/portfolio/Portfolio.tsx
+++ src/portfolio/Portfolio.tsx
@@ -3,13 +3,23 @@ function calculateTimeBetweenTwoStringDates(date1: string) {
     const dateNow: Date = new Date();
     const diffTime = Math.abs(dateNow.getTime() - date.getTime());
     const diffMos = Math.ceil(diffTime / (1000 * 60 * 60 * 24 * 30));
-    return diffMos + " mos";
+    if (diffMos > 12) {
+        return (
+            " " +
+            Math.floor(diffMos / 12) +
+            " yrs" +
+            " " +
+            (diffMos % 12) +
+            " mos"
+        );
+    }
+    return " " + diffMos + " mos";
 }
 
 function Portfolio() {
     return (
         <div className="flex-1 overflow-y-auto p-4 bg-neutral-900">
-            <h1 className="text-2xl ml-6 pt-7 text-white">About Me</h1>
+            <h1 className="text-2xl ml-6 pt-3 text-white">About Me</h1>
             <p className="top-10 ml-6 pt-5 left-3 text-white bdg-gradient-to-tr:bg-slate-300 text-sm">
                 Currently persuing my Bachelor&apos;s in Computer Science (
                 <a
@@ -51,81 +61,54 @@ function Portfolio() {
                                 href="https://www.zivver.com/"
                                 className="text-slate-400"
                             >
-                                Zivver
-                            </a>{" "}
+                                Zivver&nbsp;
+                            </a>
                             · Internship
                         </p>
                         <p className="text-xs">
-                            Mar 2024 - Present ·{" "}
+                            Mar 2024 - Present ·
                             {calculateTimeBetweenTwoStringDates("2024-03-01")}
                         </p>
                         <ul className="pl-4 pt-1">
                             <li>
-                                • Led the performance optimization of key
-                                software systems, focusing on enhancing both
-                                frontend and backend functionalities to boost
-                                overall efficiency.
+                                <p>
+                                    • Led the performance optimization of key
+                                    software systems, focusing on enhancing both
+                                    frontend and backend functionalities to
+                                    boost overall efficiency.
+                                </p>
                             </li>
                             <li>
-                                • Directed the design and implementation of user
-                                interface improvements, significantly elevating
-                                user experience across software platforms.
+                                <p>
+                                    • Directed the design and implementation of
+                                    user interface improvements, significantly
+                                    elevating user experience across software
+                                    platforms.
+                                </p>
                             </li>
                             <li>
-                                • Engineered and implemented scripts and tools
-                                for real-time performance evaluation of critical
-                                operational rules in production environments.
+                                <p>
+                                    • Engineered and implemented scripts and
+                                    tools for real-time performance evaluation
+                                    of critical operational rules in production
+                                    environments.
+                                </p>
                             </li>
                             <li>
-                                • Developed an internal analytical tool to
-                                monitor and analyze trigger events based on
-                                input data, enhancing strategic operational
-                                insights.
+                                <p>
+                                    • Developed an internal analytical tool to
+                                    monitor and analyze trigger events based on
+                                    input data, enhancing strategic operational
+                                    insights.
+                                </p>
                             </li>
                             <li>
-                                • Collaborated with cross-functional team
-                                members to ensure seamless integration of
-                                performance improvements into existing systems.
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-                <li className="list-item pt-1 pl-3">
-                    <div className="inline text-xs">
-                        <p className="text-base">Software Engineer Intern</p>
-                        <p className="text-sm">
-                            <a
-                                href="https://www.dvbcontrol.com/"
-                                className="text-slate-400"
-                            >
-                                DVBControl
-                            </a>{" "}
-                            · Internship
-                        </p>
-                        <p className="text-xs">Aug 2023 - Oct 2023 · 3 mos</p>
-                        <ul className="pl-4 pt-1">
-                            <li>
-                                •Collaborated with the development team to
-                                create a web portal for DVB Control&apos;s
-                                broadcasting monitoring tool.
-                            </li>
-                            <li>
-                                •Utilized the Angular framework to design and
-                                implement user-friendly web applications.
-                            </li>
-                            <li>
-                                •Assisted in the analysis of databases to
-                                enhance data storage and retrieval efficiency.
-                            </li>
-                            <li>
-                                •Assisted in implementing security measures to
-                                safeguard sensitive data and protect against
-                                potential vulnerabilities.
-                            </li>
-                            <li>
-                                •Assisted in resolving technical challenges,
-                                ensuring the project met performance and
-                                security standards.
+                                <p>
+                                    • Collaborated with cross-functional team
+                                    members to ensure seamless integration of
+                                    performance improvements into existing
+                                    systems.
+                                </p>
                             </li>
                         </ul>
                     </div>
@@ -138,34 +121,97 @@ function Portfolio() {
                                 href="https://sugo.nl/"
                                 className="text-slate-400"
                             >
-                                SUGO Pizza al Taglio
-                            </a>{" "}
+                                SUGO Pizza al Taglio&nbsp;
+                            </a>
                             · Part-time
                         </p>
                         <p className="text-xs">
-                            Jan 2023 - Present ·{" "}
+                            Jan 2023 - Present ·
                             {calculateTimeBetweenTwoStringDates("2023-01-01")}
                         </p>
                         <ul className="pl-4 pt-1">
                             <li>
-                                • Expertly prepared and cooked a wide range of
-                                Italian-style pizzas to perfection, ensuring
-                                consistency in taste and presentation.
+                                <p>
+                                    • Expertly prepared and cooked a wide range
+                                    of Italian-style pizzas to perfection,
+                                    ensuring consistency in taste and
+                                    presentation.
+                                </p>
                             </li>
                             <li>
-                                • Maintained a clean and organized kitchen,
-                                adhering to food safety and hygiene standards.
-                                Demonstrated proficiency in working with pizza
-                                ovens and other kitchen equipment.
+                                <p>
+                                    • Maintained a clean and organized kitchen,
+                                    adhering to food safety and hygiene
+                                    standards. Demonstrated proficiency in
+                                    working with pizza ovens and other kitchen
+                                    equipment.
+                                </p>
                             </li>
                             <li>
-                                • Coordinated and supervised fellow staff
-                                members during shifts, ensuring smooth
-                                restaurant operations.
+                                <p>
+                                    • Coordinated and supervised fellow staff
+                                    members during shifts, ensuring smooth
+                                    restaurant operations.
+                                </p>
                             </li>
                             <li>
-                                • Fostered a collaborative and positive work
-                                environment, promoting teamwork and staff morale
+                                <p>
+                                    • Fostered a collaborative and positive work
+                                    environment, promoting teamwork and staff
+                                    morale
+                                </p>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li className="list-item pt-1 pl-3">
+                    <div className="inline text-xs">
+                        <p className="text-base">Software Engineer Intern</p>
+                        <p className="text-sm">
+                            <a
+                                href="https://www.dvbcontrol.com/"
+                                className="text-slate-400"
+                            >
+                                DVBControl&nbsp;
+                            </a>
+                            · Internship
+                        </p>
+                        <p className="text-xs">Aug 2023 - Oct 2023 · 3 mos</p>
+                        <ul className="pl-4 pt-1">
+                            <li>
+                                <p>
+                                    •Collaborated with the development team to
+                                    create a web portal for DVB Control&apos;s
+                                    broadcasting monitoring tool.
+                                </p>
+                            </li>
+                            <li>
+                                <p>
+                                    •Utilized the Angular framework to design
+                                    and implement user-friendly web
+                                    applications.
+                                </p>
+                            </li>
+                            <li>
+                                <p>
+                                    •Assisted in the analysis of databases to
+                                    enhance data storage and retrieval
+                                    efficiency.
+                                </p>
+                            </li>
+                            <li>
+                                <p>
+                                    •Assisted in implementing security measures
+                                    to safeguard sensitive data and protect
+                                    against potential vulnerabilities.
+                                </p>
+                            </li>
+                            <li>
+                                <p>
+                                    •Assisted in resolving technical challenges,
+                                    ensuring the project met performance and
+                                    security standards.
+                                </p>
                             </li>
                         </ul>
                     </div>
@@ -178,28 +224,36 @@ function Portfolio() {
                                 href="https://jackastors.com/"
                                 className="text-slate-400"
                             >
-                                Jack Astor&apos;s Bar and Grill
-                            </a>{" "}
+                                Jack Astor&apos;s Bar and Grill&nbsp;
+                            </a>
                             · Part-time
                         </p>
                         <p className="text-xs">Feb 2022 - Sep 2022 · 8 mos</p>
                         <ul className="pl-4 pt-1">
                             <li>
-                                • Skilled in delivering outstanding service by
-                                greeting and serving customers.
+                                <p>
+                                    • Skilled in delivering outstanding service
+                                    by greeting and serving customers.
+                                </p>
                             </li>
                             <li>
-                                • Strong customer service attitude,
-                                self-reliant, dependable, able to prioritize,
-                                and multitask.
+                                <p>
+                                    • Strong customer service attitude,
+                                    self-reliant, dependable, able to
+                                    prioritize, and multitask.
+                                </p>
                             </li>
                             <li>
-                                • Experience working as a leader and able to
-                                work well with others to accomplish a goal.
+                                <p>
+                                    • Experience working as a leader and able to
+                                    work well with others to accomplish a goal.
+                                </p>
                             </li>
                             <li>
-                                • Experienced with handling of high volume of
-                                customers.
+                                <p>
+                                    • Experienced with handling of high volume
+                                    of customers.
+                                </p>
                             </li>
                         </ul>
                     </div>
@@ -209,7 +263,7 @@ function Portfolio() {
             <h1 className="text-2xl ml-6 pt-1 left-3 text-white">Projects</h1>
             <p className="ml-6 pt-5 left-3 text-white bg-gradient-to-tr:bg-slate-300 text-sm">
                 Some projects I&apos;ve worked on throughout the years listed
-                bellow
+                bellow.
             </p>
             <ul className="text-white list-inside ml-3 pt-2">
                 <li className="list-item pt-1 pl-3">
@@ -218,7 +272,7 @@ function Portfolio() {
                         className="text-slate-400"
                     >
                         Soluna
-                    </a>{" "}
+                    </a>
                     - Web based weather application built with Angular framework
                     and ran on a NodeJs servers
                 </li>
