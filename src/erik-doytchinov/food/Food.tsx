1909f2be72d27c7e465353a865f8f62656ab1392
Erik Doytchinov <erik.doytchinov@gmail.com>
2024-06-01 18:58:28 +0200

    Add a page not ready page.

--- src/erik-doytchinov/food/Food.tsx
+++ src/erik-doytchinov/food/Food.tsx
@@ -1,7 +1,9 @@
+import InProgress from "../inprogress/InProgress";
+
 function Food() {
     return (
-        <div className="flex-1 md:overflow-y-auto p-4 md:mt-0 md:ml-64 text-white bg-neutral-900">
-            <h1>Food</h1>
+        <div className="flex-1 md:overflow-y-auto p-4 md:mt-0 md:ml-64 text-erik.doytchinov-500 bg-erik.doytchinov-100">
+            <InProgress />
         </div>
     );
 }
---
+++ src/erik-doytchinov/inprogress/InProgress.tsx
@@ -0,0 +1,16 @@
+function InProgress() {
+    return (
+        <div className="flex items-center justify-center h-full bg-erik.doytchinov-300">
+            <div className="text-center">
+                <div className="text-5xl font-bold mb-4">🚧</div>
+                <h1 className="text-4xl font-bold mb-2">Page Not Ready Yet</h1>
+                <p className="text-lg text-gray-700">
+                    I'm working on developing this page. Please check back
+                    later.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default InProgress;
