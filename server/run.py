8d655b2dd848f2e2d8fde8324ca506398bbae375
ErikDoytchinov <erik.doytchinov@gmail.com>
2024-06-21 23:21:22 +0200

    Add gunicorn and nginx configs

--- server/run.py
+++ server/run.py
@@ -16,8 +16,18 @@ def serve():
     return send_from_directory(app.static_folder, "index.html")
 
 
+@app.route('/<path:path>')
+def serve_static(path):
+    """
+    Serve static files from the React build directory.
+    """
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+
 def main():
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0")
 
 
 if __name__ == "__main__":
